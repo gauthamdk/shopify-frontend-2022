88223335a96af8eac257ce431b2cd3b05d9bf28d
Gautham <gauthamkumar.gk.gk@gmail.com>
2022-01-15 23:45:39 +0400

    adding spinning animation

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 export default function Loading() {
-  return <div style={{ color: "white" }}>Loading...</div>;
+  return (
+    <>
+      <FontAwesomeIcon
+        icon={faSpinner}
+        size="6x"
+        style={{ color: "white" }}
+        className="fa-spin"
+      />
+      <h3 style={{ marginTop: "10px", color: "white", textAlign: "center" }}>
+        Loading...
+      </h3>
+    </>
+  );
 }
